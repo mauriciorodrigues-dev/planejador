259b7c49ca09fe76aa969d595d9de13917fde657
Mauricio Rodrigues <143841940+mauriciorodrigues-dev@users.noreply.github.com>
2024-03-22 15:41:32 -0300

    modal

--- assets/js/pages/index/index.js
+++ assets/js/pages/index/index.js
@@ -1,11 +1,22 @@
 /*
 NAVBAR - CÓDIGO
+responsividade NavBar
 */
 
 /*
 BTN-MODAL - CÓDIGO
 acessa o modal que contém o formulário de login
 */
-function teste() {
-  alert('Blz!! Você chegou atá aqui!!!!!')
-}
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+  modal.classList.toggle("hide");
+  fade.classList.toggle("hide");
+};
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+  el.addEventListener("click", () => toggleModal());
+});
